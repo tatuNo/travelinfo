d049e8bf1abeda5fb2cd832e643037fabb80462d
tatu_ <Tatu.Nordstrom@metropolia.fi>
2020-02-25 14:06:53 +0200

    flag slideshow

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
+
 function popular () {
     fetch('https://restcountries.eu/rest/v2/alpha/col')             
     .then(function(vastaus){        
       return vastaus.json();        
     }).then(function(json){         
         console.log(json);
-        let p = document.getElementById("countryName1");
+        let p = document.getElementById("name1");
         let img = document.getElementById("flag1");
 
         p.innerText = json.name;
@@ -15,7 +16,7 @@ function popular () {
       return vastaus.json();        
     }).then(function(json){         
         console.log(json);
-        let p = document.getElementById("countryName2");
+        let p = document.getElementById("name2");
         let img = document.getElementById("flag2");
 
         p.innerText = json.name;
@@ -26,11 +27,38 @@ function popular () {
       return vastaus.json();        
     }).then(function(json){         
         console.log(json);
-        let p = document.getElementById("countryName3");
+        let p = document.getElementById("name3");
         let img = document.getElementById("flag3");
 
         p.innerText = json.name;
         img.src = json.flag;
       });
     }
-popular();               
+    popular();
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}               
