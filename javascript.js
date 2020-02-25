6e108fb9be7668a1de808cffd417685fca56ef78
tatu_ <Tatu.Nordstrom@metropolia.fi>
2020-02-25 12:37:50 +0200

    flag update

--- javascript.js
+++ javascript.js
@@ -4,11 +4,33 @@ function popular () {
       return vastaus.json();        
     }).then(function(json){         
         console.log(json);
-        let p = document.getElementById("countryName");
+        let p = document.getElementById("countryName1");
         let img = document.getElementById("flag1");
 
         p.innerText = json.name;
         img.src = json.flag;
     });
-}
+    fetch('https://restcountries.eu/rest/v2/alpha/usa')             
+    .then(function(vastaus){        
+      return vastaus.json();        
+    }).then(function(json){         
+        console.log(json);
+        let p = document.getElementById("countryName2");
+        let img = document.getElementById("flag2");
+
+        p.innerText = json.name;
+        img.src = json.flag;
+    });
+    fetch('https://restcountries.eu/rest/v2/alpha/fin')             
+    .then(function(vastaus){        
+      return vastaus.json();        
+    }).then(function(json){         
+        console.log(json);
+        let p = document.getElementById("countryName3");
+        let img = document.getElementById("flag3");
+
+        p.innerText = json.name;
+        img.src = json.flag;
+      });
+    }
 popular();               
