4ea6dcacf4deb1d98574ff33ffd19db1113e6617
nicoja <nico.jarvinen2@metropolia.fi>
2020-02-25 18:10:49 +0200

    fixed responsivity and added dummy TBA area

--- javascript.js
+++ javascript.js
@@ -36,29 +36,35 @@ function popular () {
     }
     popular();
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+  	// contain images in an array
+    var image = ['','https://picsum.photos/1920/1080', 'https://picsum.photos/1920/1080', 'https://picsum.photos/1920/1080','https://picsum.photos/1920/1080'];
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}               
+    var i = image.length;
+
+    // function for next slide 
+
+    function nextImage(){
+    	if (i<image.length) {
+    		i= i+1;
+    	}else{
+    		i = 1;
+    	}
+    	var slider = document.getElementById('slider-image').innerHTML = "<img src=" + image[i-1] + ">";
+    }
+
+
+    // function for prew slide
+
+    function prewImage(){
+
+    	if (i<image.length+1 && i>1) {
+    		i= i-1;
+    	}else{
+    		i = image.length;
+    	}
+      var slider = document.getElementById('slider-image').innerHTML = "<img src=" + image[i-1] + ">";
+
+    }
