a079d9f6379ee4ca76053f57e4c7c196fcc1e19f
nicoja <nico.jarvinen2@metropolia.fi>
2020-03-03 13:53:43 +0200

    login made better

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -10,79 +6,66 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(url);
+const dbName = 'test';
+const assert = require('assert');
 
-const initializePassport = require('./passport-config');
-initializePassport(
-    passport, 
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-);
+const url = 'mongodb+srv://***REMOVED***:***REMOVED***@webtekniikka-projekti-raull.mongodb.net/test?retryWrites=true&w=majority';
 
-const users = [];
+var PORT = process.env.PORT || 3000;
 
-//tell express to find all render files in views and scripts like js and css in scripts folder.
 app.use(express.static(path.join(__dirname, '/views')));
 app.use(express.static(path.join(__dirname, '/scripts')));
 
-app.use(express.urlencoded({ extended: false}));
-
-app.use(flash());
-app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/', checkAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'), {name: req.user.name})
+router.get('/', function(req, res, next) {
+    res.render('views/index.html');
 });
 
-app.get('/register.html', checkNotAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname + '/register.html'))
-});
-
-app.get('/login.html', checkNotAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname + '/login.html'))
-});
-
-app.post('/login.html', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login.html',
-    failureFlash: true
-}));
-
-app.post('/register.html', checkNotAuthenticated, async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.redirect('/login.html');
-    } catch {
-        res.redirect('/register.html');
+router.get('/get-data', function(req, res, next){
+    var resultArray = [];
+    MongoClient.connect(url, function(err, client){
+      assert.equal(null, err);
+      const db = client.db(dbName);
+      var cursor = db.collection('user-data').find();
+      cursor.forEach(function(doc, err) {
+          assert.equal(null, err);
+          resultArray.push(doc);
+      }, function(){
+        client.close();
+        res.render('index', {items: resultArray});
+      });
+    });
+  });
+  
+  router.post('/insert', function(req, res, next) {
+    var item = {
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author
     }
-    console.log(users);
+    MongoClient.connect(url, function(err, client){
+      assert.equal(null, err);
+      const db = client.db(dbName);
+      db.collection('user-data').insertOne(item, function(err, result){
+        assert.equal(null, err);
+        console.log('Item inserted');
+        client.close();
+      });
+    });
+    res.redirect('/');
+  });
+  
+ 
+
+router.post('/update', function(req, res, next){
+
 });
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
+router.post('/delete', function(req, res, next) {
 
-    res.redirect('/login.html');
-}
+});
 
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        res.redirect('/');
-    }
-    next();
-}
+app.listen(PORT);
 
-app.listen(process.env.PORT);
+module.exports = router;
